5be32d91a96901bbc2d3fd35f41f482ee568989e
MEerYOUer <brian.michael,kanode@gmail.com>
2018-12-17 15:17:45 -0500

    Updated StartingUserInterface.cs
    Fixed interpolation on line 55.

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -52,7 +52,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine("{0} is not a suported type of gradebook, please try again",type);
+                Console.WriteLine($"{0} is not a suported type of gradebook, please try again",type);
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
